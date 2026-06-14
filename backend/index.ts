5baa3e80ac1bf43a0c274041162a59df65b475db
Saket Kumar <madmansaketkumar@gmail.com>
2026-06-14 22:27:04 +0530

    ran server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,20 @@
-console.log("hello nodemon")
+import express from "express"
+import cors from "cors"
+import http from "http"
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req,res)=> {
+        res.send("srver is fine")
+})
+
+const PORT = process.env.PORT || 3000
+const server  = http.createServer(app)
+server.listen(PORT,()=> {
+    console.log("server is running", PORT)
+})
