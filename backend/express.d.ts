17b78244a711f5f40f88c2f35ef7e621d086c82f
Saket Kumar <madmansaketkumar@gmail.com>
2026-06-18 23:59:31 +0530

    fixed types

--- backend/express.d.ts
+++ backend/express.d.ts
@@ -1,5 +1,9 @@
-declare namespace Express {
-  export interface Request {
+// express.d.ts
+
+import "express";
+
+declare module "express-serve-static-core" {
+  interface Request {
     user?: {
       userId: string;
     };
