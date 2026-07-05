2229a4043e2d4bfdbac954e30e931367876ae583
Saket Kumar <madmansaketkumar@gmail.com>
2026-07-05 22:41:02 +0530

    fixed folder  structures

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,29 @@
-import { View, Text } from "react-native";
+import {   useRouter } from "expo-router";
+import { View, Text, Button } from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-black px-6">
-      <Text className="text-4xl font-bold text-emerald-400">Talk-On</Text>
+    const router = useRouter();
 
-      <Text className="mt-4 text-center text-gray-300">
-        Welcome to your AI voice assistant app.
-      </Text>
+    return (
+      <View className="flex-1 items-center justify-center bg-black px-6">
+        <Text className="text-4xl font-bold text-emerald-400">Talk-On</Text>
 
-      <View className="mt-8 rounded-2xl bg-zinc-900 p-6 w-full">
-        <Text className="text-xl font-semibold text-white">
-          Start a Conversation
+        <Text className="mt-4 text-center text-white-300">
+          Welcome to your AI voice assistant app.
         </Text>
 
-        <Text className="mt-2 text-gray-400">
-          Tap the microphone button and begin chatting with AI.
-        </Text>
+        <View className="mt-8 rounded-2xl bg-zinc-900 p-6 w-full">
+          <Text
+            className="text-xl font-semibold text-white"
+            onPress={() => router.push("/auth/sign-in")}
+          >
+            Start a Conversation
+          </Text>
+
+          <Text className="mt-2 text-gray-400">
+            Tap the microphone button and begin chatting with AI.
+          </Text>
+        </View>
       </View>
-    </View>
-  );
+    );
 }
---
+++ app/auth/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+ 
+
+const signin = () => {
+  return (
+    <View className='flex-1 flex min-h-screen justify-center bg-black items-center'>
+      <Text className='text-white font-bold'>sign-in</Text>
+    </View>
+  )
+}
+
+export default signin
