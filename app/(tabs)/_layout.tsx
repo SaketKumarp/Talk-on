90f7b4f0889436ef5bdca4ae43ff18e7be519ced
Saket Kumar <madmansaketkumar@gmail.com>
2026-07-05 22:25:44 +0530

    added previous files

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#0a0a0a",
+          borderTopWidth: 0,
+          height: 60,
+        },
+        tabBarActiveTintColor: "#e67e22",
+        tabBarInactiveTintColor: "#888",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="time" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
